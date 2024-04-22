0cea7ae9e47bd9615d0742c7122708e299d80757
Yujiro Akihiro <yujiro.akihiro@gmail.com>
2024-04-22 15:39:58 +0200

    done*

--- index.js
+++ index.js
@@ -1,17 +1,64 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = []; // add new array
+    this.length = 0; // initialized as number
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item); // add item to items
+    this.items.sort((a, b) => a - b); // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/sort
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos < 0) { // less than 0 => Error
+      throw new Error("OutOfBounds");
+    }
+    if (pos >= this.length) { // bigger than index number => Error
+      throw new Error("OutOfBounds");
+    }
+    return this.items[pos];
+  }
 
-  max() {}
+  max() {
+    if (this.length === 0) { // check inputs
+      throw new Error(`EmptySortedList`);
+    }
+    return this.items[this.length - 1]; // return => max index number.
+  }
 
-  min() {}
+  min() { 
+    if (this.length === 0) { // check inputs
+      throw new Error(`EmptySortedList`);
+    }
+    return this.items[0]; // return => min index number.
+  
+  }
 
-  sum() {}
+  sum() { 
+    return this.items.reduce((acc, cur) => acc + cur, 0);// acc first initialize as 0 & sum up all value in the array
+    // basic one
+    // let sum = 0;
+    // for (let i = 0; i < this.length; i++) {
+    //   sum += this.items[i];
+    // }
+    // return this.sum; // :-)
+  }
 
-  avg() {}
+  avg() { 
+    if (this.length === 0) {
+      throw new Error("EmptySortedList");
+    }
+    return this.sum() /this.length; // call sum() to get sum.
+   
+    // or classic
+    // let sum = 0;
+    // for (let i = 0; i < this.items.length; i++){
+    // sum += this.items[i];
+    // }
+    // return sum / this.length;
+    // }
+  }
 }
 
 module.exports = SortedList;
